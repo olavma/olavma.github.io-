f8807ec3fd30f3aa890d72eb63a89cc1098a4b00
NickfGe <olavmar2004@gmail.com>
2024-07-18 21:42:38 +0200

    Funcion de Jugar

--- src/scripts/getProject.js
+++ src/scripts/getProject.js
@@ -16,7 +16,38 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function setInfo(proj){
-    const cont = document.getElementById("container");
-    cont.textContent = proj.title;
     document.title = proj.title;
+
+    // Nombre
+    var title = document.getElementsByTagName("h4")[0];
+    title.textContent = proj.title;
+
+    // Portada
+    var img = document.getElementsByTagName("img")[0];
+    img.src = proj.logo;
+    img.alt = proj.logoAlt;
+
+    // Sinopsis
+    var sinopsisArt = document.querySelector('.sinopsis');
+    var sinopsisP = sinopsisArt.querySelector('p');
+    sinopsisP.textContent = proj.body;
+
+    // Idiomas
+    var lang = document.getElementById("idioma");
+    lang.textContent = proj.lang;
+
+    // Boton + info
+    var btn = document.getElementById('infoBtn');
+    btn.href = proj.github;
+
+    // Boton Jugar
+    if(proj.play){
+        var gameBtn = document.getElementById("gameBtn");
+        gameBtn.href = proj.play;
+        gameBtn.classList.remove("hidden");
+    }
+    // if(proj.Jugar){
+    //     btn[1].href = proj.juego + "?id=" + proj.Idprojo;
+    //     btn[1].style.display = "block";
+    // }
 }
